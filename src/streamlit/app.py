bfeeede4c122629ce61d38b986565220bd5448e8
Reda Guemry <guemryreda@gmail.com>
2026-09-18 23:53:27 +0100

    add the other columns

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -9,19 +9,43 @@ from src.database import (
 
 session = sessionFactory()
 
-stmt = (
+nombre_citie = session.scalar(
     select(
         func.count(City.id) 
     )
 )
 
-nombre_citie = session.scalar(stmt)
+max_temperature = session.scalar(
+    select(
+        func.max(WeatherForecast.temperature_2m_max)
+    )
+)
+
+max_precipitation = session.scalar(
+    select(
+        func.max(WeatherForecast.precipitation_sum)
+    )
+)
+
+nombre_periodes_risque = session.scalar(
+    select(
+        func.count(WeatherForecast.id)
+    )
+    .where(WeatherForecast.risk_score > 0)
+)
+
+ville_risque_eleve = session.scalar(
+    select(City.name)
+    .join(WeatherForecast, WeatherForecast.city_id == City.id)
+    .order_by(WeatherForecast.risk_score.desc())
+    .limit(1)
+)
 
 col1, col2, col3, col4, col5 = st.columns(5)
 
 col1.metric("Cities", nombre_citie)
-col2.metric("Max Temperature", "38.5 °C")
-col3.metric("Max Precipitation", "42 mm")
-col4.metric("Risk Periods", 12)
-col5.metric("Highest Risk City", "Marrakech")
+col2.metric("Max Temperature", f"{max_temperature} °C")
+col3.metric("Max Precipitation", f"{max_precipitation} mm")
+col4.metric("Risk Periods", nombre_periodes_risque)
+col5.metric("Highest Risk City", ville_risque_eleve)
 
